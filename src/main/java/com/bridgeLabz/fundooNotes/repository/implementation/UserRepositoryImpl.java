59ae15b13c1293c4b722553665a54ec35d7b648b
durgasankar <durgasankar.raja500@gmail.com>
2020-01-21 10:08:00 +0530

    [durgasankar].add -> registration-repo service save opeartion and fetch by email functionality added

--- src/main/java/com/bridgeLabz/fundooNotes/repository/implementation/UserRepositoryImpl.java
+++ src/main/java/com/bridgeLabz/fundooNotes/repository/implementation/UserRepositoryImpl.java
@@ -1,20 +1,36 @@
 package com.bridgeLabz.fundooNotes.repository.implementation;
 
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.bridgeLabz.fundooNotes.model.User;
 import com.bridgeLabz.fundooNotes.repository.IUserRepository;
 
+@Repository
 public class UserRepositoryImpl implements IUserRepository {
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@Override
 	public User save(User newUser) {
-		// TODO Auto-generated method stub
-		return null;
+		// got the current session from entity manager
+		Session session = entityManager.unwrap(Session.class);
+		// from the existing session did hibernate operation
+		session.saveOrUpdate(newUser);
+		return newUser;
 	}
 
 	@Override
 	public User getUser(String emailId) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = entityManager.unwrap(Session.class);
+		Query emailFetchQuery = session.createQuery("FROM User where email=:emailId");
+		emailFetchQuery.setParameter("email", emailId);
+		return (User) emailFetchQuery.uniqueResult();
 	}
 
 }
